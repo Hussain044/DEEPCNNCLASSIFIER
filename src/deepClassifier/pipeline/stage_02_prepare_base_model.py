c4cecabd5136373abcc4a166a6701464e022bef4
Hussain044 <mohdishtiaq044@gmail.com>
2022-10-31 15:09:22 +0100

    stage-2 files updated

---
+++ src/deepClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,10 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import PrepareBaseModel
+try:
+    config = ConfigurationManager()
+    prepare_base_model_config = config.get_prepare_base_model_config()
+    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+except Exception as e:
+    raise e
